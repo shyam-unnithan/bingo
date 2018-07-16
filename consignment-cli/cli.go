2fbd0a2fc353e3471ab9ddfc4728edf892ac168d
Shyam Unnithan <shyam.unnithan@gmail.com>
2018-07-16 23:28:27 +0530

    Updated with 1) Consignment client which invokes the consignment service 2) Consignment service which provides information on the consignments 3) Vessel service which provides data on vessels
    All services are grpc microservices using the golang micro framework

---
+++ consignment-cli/cli.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
+	pb "github.com/shyam-unnithan/bingo/consignment-service/proto/consignment"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	cmd.Init()
+	client := pb.NewShippingServiceClient("bingo.svc.consignment", microclient.DefaultClient)
+
+	file := defaultFilename
+	consignment, err := parseFile(file)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+}
---
+++ consignment-service/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	micro "github.com/micro/go-micro"
+	pb "github.com/shyam-unnithan/bingo/consignment-service/proto/consignment"
+	vesselProto "github.com/shyam-unnithan/bingo/vessel-service/proto/vessel"
+	"golang.org/x/net/context"
+)
+
+const (
+	port = ":50051"
+)
+
+type Repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
+}
+
+type ConsignmentRepository struct {
+	consignments []*pb.Consignment
+}
+
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	return consignment, nil
+}
+
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+type service struct {
+	repo         Repository
+	vesselClient vesselProto.VesselServiceClient
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	// We set the VesselId as the vessel we got back from our
+	// vessel service
+	req.VesselId = vesselResponse.Vessel.Id
+
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	res.Created = true
+	res.Consignment = consignment
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
+func main() {
+	repo := &ConsignmentRepository{}
+
+	srv := micro.NewService(
+		// This name must match the package name given in your protobuf definition
+		micro.Name("bingo.svc.consignment"),
+		micro.Version("latest"),
+	)
+
+	vesselClient := vesselProto.NewVesselServiceClient("bingo.svc.vessel", srv.Client())
+
+	// Init will parse the command line flags.
+	srv.Init()
+
+	// Register handler
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
+
+	// Run the server
+	if err := srv.Run(); err != nil {
+		fmt.Println(err)
+	}
+
+}
